7997a23b66f0bc3e0f8fc7cffcbc69d791529484
AJ Marino <aj@ajmarino.com>
2013-11-17 21:19:40 -0500

    minor style fix, adds google analytics
    - minor style fixes
    - renames header and footer files from .inc to .php
    - adds google analytics tracking code

--- app/public/index.php
+++ app/public/index.php
@@ -1,9 +1,9 @@
 <?php 
-	@include "templates/head.inc";
+	@include "templates/head.php";
 ?>
 
 <section class="main-content" role="main" ng-view></section>
 
 <?php
-	@include "templates/footer.inc";
+	@include "templates/footer.php";
 ?>
